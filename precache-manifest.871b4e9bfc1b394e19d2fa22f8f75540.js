2b4496961af3a3809a20ab63a3a0b8a22f75e660
Darryl Mendonez <darryl.mendonez@nucleusglobal.com>
2020-08-16 02:20:21 -0400

    Updates

---
+++ precache-manifest.871b4e9bfc1b394e19d2fa22f8f75540.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5785bbee7fe2b1cd7f84ce8ed76d2a5f",
+    "url": "/barebones-react-todo-list/index.html"
+  },
+  {
+    "revision": "d15de524ba2236dc1f04",
+    "url": "/barebones-react-todo-list/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "5d5baa6904f63ed750f7",
+    "url": "/barebones-react-todo-list/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "d15de524ba2236dc1f04",
+    "url": "/barebones-react-todo-list/static/js/2.0d9957f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/barebones-react-todo-list/static/js/2.0d9957f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d5baa6904f63ed750f7",
+    "url": "/barebones-react-todo-list/static/js/main.d8f0ef93.chunk.js"
+  },
+  {
+    "revision": "0fd7f6bf08778f35b3b8",
+    "url": "/barebones-react-todo-list/static/js/runtime-main.91874576.js"
+  }
+]);
