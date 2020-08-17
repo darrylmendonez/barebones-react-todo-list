3f5af800e44d89f9330f942a91637269e32f4c40
Darryl Mendonez <darryl.mendonez@nucleusglobal.com>
2020-08-16 22:29:48 -0400

    reverted back to browserrouter and updated homepage to gh url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './components/home'
 import ClassComponent from './components/classComponent'
@@ -8,7 +8,7 @@ import './App.css';
 
 const App = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <div className="App">
         <Switch>
           <Route exact path='/' component={Home} />
@@ -16,7 +16,7 @@ const App = () => {
           <Route path='/function-component' component={FunctionComponent} />
         </Switch>
       </div>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
